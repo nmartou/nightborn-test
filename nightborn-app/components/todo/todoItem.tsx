93cefa982d49cc23626aabb9797ae21b35e5d6ff
[Nicolas] <martounicolas@gmail.com>
2025-12-10 10:32:25 +0100

    refactor(todoItem): use unpacketed id instead of all object

--- nightborn-app/components/todo/todoItem.tsx
+++ nightborn-app/components/todo/todoItem.tsx
@@ -7,8 +7,8 @@ import type { ItemProps } from '@/types/todoList.type'
 export default function TodoItem(props: {data: ItemProps}) {
     const { id, title, completed, created_at } = props.data;
     return(
-        <Link href={`/${props.data.id}`}>
-            <article className="border border-black rounded-lg mb-5 p-5 flex align-middle justify-between">
+        <Link href={`/${id}`}>
+            <article className="border border-black rounded-lg mb-5 p-5 flex align-middle justify-between hover:scale-102 transition-transform">
                 <div className="flex gap-3">
                     <h2>{title}</h2>
                     <Status completed={completed} />
