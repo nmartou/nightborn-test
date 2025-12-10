b946946c5b43a391f9fdb3bce4dbbf5feaa829ea
[Nicolas] <martounicolas@gmail.com>
2025-12-10 11:20:30 +0100

    fix(queryClient): setup global access to client

--- nightborn-app/app/page.tsx
+++ nightborn-app/app/page.tsx
@@ -1,17 +1,12 @@
 "use client"
 
 import TodoList from "@/components/todo/todoList";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 export default function Home() {
   return (
     <div>
       <main>
-        <QueryClientProvider client={queryClient}>
           <TodoList />
-        </QueryClientProvider>
       </main>
     </div>
   );
